d1e15437adec5a0ed84c8edb4e113a89c4f0b121
Markus Hedenborn <markus@formbound.com>
2022-02-12 14:24:27 +0100

    beer run push

---
+++ src/items/items.service.ts
@@ -0,0 +1,77 @@
+/**
+ * Data Model Interfaces
+ */
+
+import { BaseItem, Item } from "./item.interface";
+import { Items } from "./items.interface";
+
+/**
+ * In-Memory Store
+ */
+
+let items: Items = {
+  1: {
+    id: 1,
+    name: "Fäbojänsk Falukorv",
+    price: 599,
+    description: "Falukorv marinerad i svensk hederlig bakteriekultur",
+    image: "https://bit.ly/3JqK2cb",
+  },
+  2: {
+    id: 2,
+    name: " Palmes Paté",
+    price: 299,
+    description: "Mosig och spontan",
+    image: "https://bit.ly/3LmVLKK",
+  },
+  3: {
+    id: 3,
+    name: "Schymans piss",
+    price: 199,
+    description: "Besk och alltid en besvikelse",
+    image: "https://bit.ly/3LuwEpp",
+  },
+};
+
+/**
+ * Service Methods
+ */
+
+export const findAll = async (): Promise<Item[]> => Object.values(items);
+export const find = async (id: number): Promise<Item> => items[id];
+
+export const create = async (newItem: BaseItem): Promise<Item> => {
+  const id = new Date().valueOf();
+
+  items[id] = {
+    id,
+    ...newItem,
+  };
+
+  return items[id];
+};
+
+export const update = async (
+  id: number,
+  itemUpdate: BaseItem
+): Promise<Item | null> => {
+  const item = await find(id);
+
+  if (!item) {
+    return null;
+  }
+
+  items[id] = { id, ...itemUpdate };
+
+  return items[id];
+};
+
+export const remove = async (id: number): Promise<null | void> => {
+  const item = await find(id);
+
+  if (!item) {
+    return null;
+  }
+
+  delete items[id];
+};
